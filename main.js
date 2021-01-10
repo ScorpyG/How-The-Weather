5d354247975495030c57c1ac6497269ebc4107ea
Justin Hoang <69221471+ScorpyG@users.noreply.github.com>
2021-01-10 11:44:07 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
+// API info & access key
 const api = {
     key: "98f5321b281efe78f62746b1d3edc4bb",
     base: "https://api.openweathermap.org/data/2.5/"
 }
 
+// Event function for search request
 const searchbox = document.querySelector('.search-box');
 searchbox.addEventListener('keypress', setQuery);
 
+// Initiate results data after search function is requested
 function setQuery (event) {
     if (event.keyCode == 13) {
         getResults(searchbox.value)
     }
 }
 
-
+// Toggle between °C / °F measurements
 function toggleUnits() {
     let checkBox = document.getElementById('unit-cb');
     if (checkBox.checked == true) {
@@ -23,6 +26,7 @@ function toggleUnits() {
     }
 } 
 
+// Toggle symbol between °C / °F
 function toggleSym() {
     let checkBox = document.getElementById('unit-cb');
     if (checkBox.checked == true) {
@@ -33,15 +37,17 @@ function toggleSym() {
     }
 } 
 
+// Fetching data function from the API
 function getResults(query) {
     fetch(`${api.base}weather?q=${query}&units=${toggleUnits()}&APPID=${api.key}`)
         .then(weather => {
-            return weather.json();
+            // return and display the results
+            return weather.json(); 
         }).then(displayResults);
     console.log(query)
 }
 
-
+// Building date function with months and days array
 function dateBuilder(d) {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -50,24 +56,31 @@ function dateBuilder(d) {
     let date = d.getDate();
     let month = months[d.getMonth()];
     let year = d.getFullYear();
-
+    
+    //return the date for the program
     return `${day}, ${month} ${date}, ${year}`
 }
 
+// Display the requested results in HTML page
 function displayResults(weather) {
+    // Requested data for searched location
     let city = document.querySelector('.location .city');
     city.innerText = `${weather.name}, ${weather.sys.country}`;
 
+    // Set current date/time
     let now = new Date();
     let date = document.querySelector('.location .date');
     date.innerText = dateBuilder(now);
 
+    // Set current temp
     let temp = document.querySelector('.current .temp');
     temp.innerHTML = `${Math.round(weather.main.temp)}${toggleSym()}`;
-
+    
+    // Set current weather
     let weather_el=document.querySelector('.current .weather');
     weather_el.innerText = weather.weather[0].main;
 
+    // Set high and low temp
     let hilow = document.querySelector('.hi-low')
     hilow.innerText = `${Math.round(weather.main.temp_min)}${toggleSym()} / ${Math.round(weather.main.temp_max)}${toggleSym()}`
 }
